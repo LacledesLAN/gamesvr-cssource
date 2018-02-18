9ab4c7e646897bc946b0484ffec5647da6a4a028
James Dudley <james@dudley.codes>
2018-02-17 20:50:48 -0600

    Updating to multi-stage build

--- linux/ll-tests/gamesvr-cssource.sh
+++ linux/ll-tests/gamesvr-cssource.sh
@@ -1,95 +1,154 @@
 #!/bin/bash -i
-declare LLCK_LOGFILE="/app/ll-tests/gamesvr-cssource.log";
-declare LLCK_CMD="/app/srcds_run -game cstrike +map de_dust2 -insecure -tickrate 100 -norestart +sv_lan 1";
-declare LLCK_CMD_TIMEOUT=60;
 
-: > $LLCK_LOGFILE
-if [ ! -f $LLCK_LOGFILE ]; then
-    echo 'Failed to create logfile: '"$LLCK_LOGFILE"'. Verify file system permissions.';
-    exit 1;
+#####################################################################################################
+### CONFIG VARS #####################################################################################
+declare LLTEST_CMD="/app/srcds_run -game cstrike +map de_dust2 -insecure -norestart +sv_lan 1";
+declare LLTEST_NAME="gamesvr-cssource-$(date '+%H%M%S')";
+#####################################################################################################
+#####################################################################################################
+
+# Runtime vars
+declare LLCOUNTER=0;
+declare LLBOOT_ERRORS="";
+declare LLTEST_HASFAILURES=false;
+declare LLTEST_LOGFILE="$LLTEST_NAME"".log";
+
+# Server log file should contain $1 because $2
+function should_have() {
+    if ! grep -i -q "$1" "$LLTEST_LOGFILE"; then
+        echo $"[FAIL] - '$2'";
+        LLTEST_HASFAILURES=true;
+    else
+        echo $"[PASS] - '$2'";
+    fi;
+}
+
+# Server log file should NOT contain $1 because $2
+function should_lack() {
+    if grep -i -q "$1" "$LLTEST_LOGFILE"; then
+        echo $"[FAIL] - '$2'";
+        LLTEST_HASFAILURES=true;
+    else
+        echo $"[PASS] - '$2'";
+    fi;
+}
+
+# Command $1 should make server return $2
+function should_echo() {
+    tmux has-session -t "$LLTEST_NAME" 2>/dev/null;
+    if [ "$?" == 0 ] ; then
+        LLCOUNTER=0;
+        LLTMP=$(md5sum "$LLTEST_LOGFILE");
+        tmux send -t "$LLTEST_NAME" C-z "$1" Enter;
+
+        while true; do
+            sleep 0.5;
+
+            if  (( "$LLCOUNTER" > 30)); then
+                echo $"[FAIL] - Command '$!' TIMED OUT";
+                LLTEST_HASFAILURES=true;
+                break;
+            fi;
+
+            if [[ $(md5sum "$LLTEST_LOGFILE") != "$LLTMP" ]]; then
+                should_have "$2" "'$1' returned expected '$2' (loop iterations: $LLCOUNTER)";
+                break;
+            fi;
+
+            (( LLCOUNTER++ ));
+        done;
+    else
+        echo $"[ERROR]- Could not run command '$1'; tmux session not found";
+        LLTEST_HASFAILURES=true;
+    fi;
+}
+
+# Check prereqs
+command -v awk > /dev/null 2>&1 || echo "awk is missing";
+command -v md5sum > /dev/null 2>&1 || echo "md5sum is missing";
+command -v sleep > /dev/null 2>&1 || echo "sleep is missing";
+command -v tmux > /dev/null 2>&1 || echo "tmux is missing";
+
+# Prep log file
+: > "$LLTEST_LOGFILE"
+if [ ! -f "$LLTEST_LOGFILE" ]; then
+    echo 'Failed to create logfile: '"$LLTEST_LOGFILE"'. Verify file system permissions.';
+    exit 2;
 fi;
 
-# $1 -> text we want to find in the log file
-# $2 -> description of why we want text to exist
-function should_have() {
-    if ! grep -i -q "$1" "$LLCK_LOGFILE"; then
-        echo $'FAIL: '"$2";
-        LLCK_HASFAILS=true;
-    else
-        echo $'PASS: '"$2";
+echo $'\n\nRUNNING TEST: '"$LLTEST_NAME";
+echo $'Command: '"$LLTEST_CMD";
+echo "Running under $(id)"$'\n';
+
+# Execute test command in tmux session
+tmux new -d -s "$LLTEST_NAME" "sleep 0.5; $LLTEST_CMD";
+sleep 0.3;
+tmux pipe-pane -t "$LLTEST_NAME" -o "cat > $LLTEST_LOGFILE";
+
+while true; do
+    tmux has-session -t "$LLTEST_NAME" 2>/dev/null;
+    if [ "$?" != 0 ] ; then
+        echo $'terminated.\n';
+        LLBOOT_ERRORS="Test process self-terminated";
+        break;
     fi;
-}
 
-# $1 -> text we *don't* want to find in the log file
-# $2 -> description of why we don't want text to exist
-function should_lack() {
-    if grep -i -q "$1" "$LLCK_LOGFILE"; then
-        echo $'FAIL: '"$2";
-        LLCK_HASFAILS=true;
-    else
-        echo $'PASS: '"$2";
-    fi;
-    return 0;
-}
-
-declare LLCK_HASFAILS=false;
-
-echo $'\n\n[RUNNING APP FOR '"$LLCK_CMD_TIMEOUT SECONDS]";
-echo $'Command: '"$LLCK_CMD";
-echo "Running under $(id)";
-
-SCREEN_RCFILE=$(mktemp);
-echo "logfile $LLCK_LOGFILE" > "$SCREEN_RCFILE";
-
-SCREEN_ID=LLCHECKS.$$.$RANDOM;
-# shellcheck disable=SC2086
-screen -c "$SCREEN_RCFILE" -m -d -L -S "$SCREEN_ID" $LLCK_CMD
-
-SCREEN_PID="$(screen -ls | grep $SCREEN_ID | awk -F . '{print $1}' | awk '{print $1}')";
-unset SCREEN_ID;
-
-if [[ ! -z "${SCREEN_PID// }" ]]; then      # Make sure screen session isn't already terminated
-    # shellcheck disable=SC2086
-    while ps -p $SCREEN_PID > /dev/null; do
-        if [ $LLCK_CMD_TIMEOUT -le 0 ]; then
-            kill -9 -"$SCREEN_PID";
-            echo "done.";
+    if  (( "$LLCOUNTER" >= 10 )); then
+        if [ -s "$LLTEST_LOGFILE" ] && ((( $(date +%s) - $(stat -L --format %Y "$LLTEST_LOGFILE") ) > 10 )); then
+            echo $'succeeded.\n';
             break;
         fi;
-        if (( LLCK_CMD_TIMEOUT % 5 == 0 )); then
-            echo -n "$LLCK_CMD_TIMEOUT...";
+
+        if (( "$LLCOUNTER" > 90 )); then
+            echo $'timed out.\n';
+            LLBOOT_ERRORS="Test timed out";
+            break;
         fi;
-        (( LLCK_CMD_TIMEOUT-- ))
-        sleep 1;
-    done
-    unset LLCK_CMD_TIMEOUT;
-fi;
+    fi;
 
-rm -f "$SCREEN_RCFILE"; unset SCREEN_RCFILE;
+    if (( LLCOUNTER % 5 == 0 )); then
+        echo -n "$LLCOUNTER...";
+    fi;
 
-screen -wipe > /dev/null;
+    (( LLCOUNTER++ ));
+    sleep 1;
+done;
 
-if [ -s $LLCK_LOGFILE ]; then               # Make sure log file isn't empty
+if [ -s "$LLTEST_LOGFILE" ]; then
     echo $'\n[LOGFILE OUTPUT]';
-    cat $LLCK_LOGFILE;
+    awk '{print "»»  " $0}' "$LLTEST_LOGFILE"
 else
-    echo $'\n\nOUTPUT LOG IS EMPTY!\n\n';
+    echo $'\nOUTPUT LOG IS EMPTY!\n';
     exit 1;
 fi;
 
-echo $'\n[RUNNING CHECKS ON LOGFILE]';
+if [ ! -z "${LLBOOT_ERRORS// }" ]; then
+    echo "Boot error: $LLBOOT_ERRORS";
+    exit 1;
+fi;
 
+echo $'\n[RUNNING CHECKS ON LOGFILE]\n';
+
+#####################################################################################################
+### TESTS ###########################################################################################
 should_lack 'Server restart in 10 seconds' 'Server is not boot-looping';
 should_lack 'Running the dedicated server as root' 'Server is not running under root';
 should_have 'server_srv.so loaded for "Counter-Strike: Source"' 'srcds_run loaded CS:Source';
 should_have 'Server is hibernating' 'srcds_run succesfully hibernated';
+should_echo "sv_cheats" '"sv_cheats" = "0"';
+#####################################################################################################
+#####################################################################################################
+
+tmux has-session -t "$LLTEST_NAME" 2>/dev/null;
+if [ "$?" == 0 ] ; then
+    tmux kill-session -t "$LLTEST_NAME";
+fi;
 
 echo $'\n\n[FINAL RESULT]';
-
-if [ $LLCK_HASFAILS = true ]; then
-    echo $'Checks have failures!\n\n\n';
+if [ $LLTEST_HASFAILURES = true ]; then
+    echo $'Checks have failures!\n\n';
     exit 1;
 fi;
 
-echo $'All checks passed!\n\n\n';
+echo $'All checks passed!\n\n';
 exit 0;
